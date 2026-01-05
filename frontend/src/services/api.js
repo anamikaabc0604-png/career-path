6d927bf55565bd8d8e83453777dc36cd438331c2
Anamika Singh <anamikabc0604@gmail.com>
2026-01-05 22:41:56 +0530

    fixing api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://career-path-backend-rfs1.onrender.com/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ||  "http://localhost:8080/api";
 
 
 export const fetchUserSkills = async (userId) => {
