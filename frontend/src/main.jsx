3294a49305a028e684dd4bf02aed7a0a8369c60e
Anamika Singh <anamikabc0604@gmail.com>
2026-01-19 20:43:45 +0530

    fix: debug API base url issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+console.log("ENV CHECK 👉", import.meta.env);
+console.log("API URL 👉", import.meta.env.VITE_API_BASE_URL);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
